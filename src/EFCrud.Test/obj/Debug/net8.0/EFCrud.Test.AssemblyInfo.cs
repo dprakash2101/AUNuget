86ce1cd5b887b7b78f34ca3f888879eeffe81a7f
dprakash2101 <dprakash2101@gmail.com>
2024-01-10 08:01:00 +0000

    Update version in .csproj

--- src/EFCrud.Test/obj/Debug/net8.0/EFCrud.Test.AssemblyInfo.cs
+++ src/EFCrud.Test/obj/Debug/net8.0/EFCrud.Test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EFCrud.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f209463427d27327b803ad78220cd1c0979b813e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5be99ccce41c8648b1b5e1c276eda7d3fd7e8107")]
 [assembly: System.Reflection.AssemblyProductAttribute("EFCrud.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EFCrud.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
